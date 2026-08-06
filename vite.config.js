f1add3f8a8dfd8dd452676251d8d5ee3550ada70
TitaniumSalmon <perapon.s@ku.th>
2026-08-07 02:45:43 +0700

    pre deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const base =
+  process.env.GITHUB_ACTIONS && !repository.endsWith('.github.io')
+    ? `/${repository}/`
+    : '/';
+
 export default defineConfig({
   plugins: [react()],
+  base,
   server: { port: 5173 },
 });
